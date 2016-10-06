d2469c10aa1312b273ca063e2c093e719c91ed67
Daniel Alzugaray <dalzuga@gmail.com>
2016-10-06 15:30:04 -0700

    implemented a line drawing and keyboard events for "Escape" key, and for the "X to close" button of the window

---
+++ demo.h
@@ -0,0 +1,16 @@
+#ifndef _DEMO_H_
+#define _DEMO_H_
+
+#include <SDL2/SDL.h>
+
+typedef struct SDL_Instance
+{
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+} SDL_Instance;
+
+void draw_stuff(SDL_Instance);
+int init_instance(SDL_Instance *);
+int poll_events(void);
+
+#endif
--- main.c
+++ main.c
@@ -1,18 +1,4 @@
-#ifndef _DEMO_H_
-#define _DEMO_H_
-
-#include <SDL2/SDL.h>
-
-typedef struct SDL_Instance
-{
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-} SDL_Instance;
-
-int init_instance(SDL_Instance *);
-#endif
-
-void draw_stuff(SDL_Instance);
+#include "demo.h"
 
 int main(void)
 {
@@ -25,42 +11,12 @@ int main(void)
 	{
 		SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
 		SDL_RenderClear(instance.renderer);
+		if (poll_events() == 1)
+			break;
 		draw_stuff(instance);
 		SDL_RenderPresent(instance.renderer);
 	}
-
-	return (0);
-}
-
-int init_instance(SDL_Instance *instance)
-{
-	/* Initialize SDL */
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	{
-		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
-		return (1);
-	}
-
-	/* Create a new window instance */
-	instance->window = SDL_CreateWindow("SDL2 \\o/", SDL_WINDOWPOS_CENTERED,
-					    SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
-
-	if (instance->window == NULL)
-	{
-		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-		return (1);
-	}
-
-	/* Create a new Renderer instance linked to the window */
-	instance->renderer = SDL_CreateRenderer(instance->window, -1,
-				      SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (instance->renderer == NULL)
-	{
-		SDL_DestroyWindow(instance->window);
-		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
-		SDL_Quit();
-		return (1);
-	}
+	
 	return (0);
 }
 
@@ -68,4 +24,25 @@ void draw_stuff(SDL_Instance instance)
 {
 	SDL_SetRenderDrawColor(instance.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderDrawLine(instance.renderer, 10, 10, 100, 100);
+}
+
+int poll_events(void)
+{
+	SDL_Event event;
+	SDL_KeyboardEvent key;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			return (1);
+		case SDL_KEYDOWN:
+			key = event.key;
+			/* If 'ESCAPE' is pressed */
+			if (key.keysym.scancode == 0x29)
+				return (1);
+			break;
+		}
+	}
 }
