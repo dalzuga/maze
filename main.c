688fa458d3955f02a5d290012a37e91e8d40069d
Daniel Alzugaray <dalzuga@gmail.com>
2016-10-07 17:03:57 -0700

    splitting drawing functions into separate file

--- main.c
+++ main.c
@@ -23,12 +23,6 @@ int main(void)
 	return (0);
 }
 
-void draw_stuff(SDL_Instance instance)
-{
-	SDL_SetRenderDrawColor(instance.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-	SDL_RenderDrawLine(instance.renderer, 10, 10, 100, 100);
-}
-
 int poll_events(void)
 {
 	SDL_Event event;
