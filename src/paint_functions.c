01e88e276bdc628b005e15c80b66e1110fc81a95
Daniel Alzugaray <dalzuga@gmail.com>
2017-04-21 04:35:34 -0700

    save

--- src/paint_functions.c
+++ src/paint_functions.c
@@ -9,6 +9,5 @@ void paint_environment(SDL_Instance instance, int **map, GamePlayer *p)
 	SDL_SetRenderDrawColor(instance.renderer, 0x80, 0x80, 0x80, 0xFF);
 	SDL_RenderDrawLine(instance.renderer, 50, 100, 200, 400);
 
-	printf("player: %p", (void *) p);
-	printf("map: %p", (void *) map);
+	printf("player: %p | map: %p\n", (void *) p, (void *) map);
 }
