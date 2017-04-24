fd94165bed025ff061187632592798c0719cb30a
Daniel Alzugaray <dalzuga@gmail.com>
2017-04-23 17:28:09 -0700

    save

--- src/paint_functions.c
+++ src/paint_functions.c
@@ -61,20 +61,17 @@ void paint_projection(SDL_Instance instance __attribute__ ((unused)),
 
 	for (i = 0; i < X_RES; i++)
 	{
-		if (i < 10)
-		{
-			d_slice = calculate_dslice(map, p, i, &px, &py);
-		}
+		d_slice = calculate_dslice(map, p, i, &px, &py);
 		/* slice_height = WALL_HEIGHT / d_slice * 277; */
 		slice_height = precompute[0] / (double) d_slice; /* see ^^ */
 		precompute[1] = slice_height / 2;
 		top = Y_CENTER + precompute[1];
 		bottom = Y_CENTER - precompute[1];
-		/* if (DEBUG == 1) */
-		/* { */
-		/* 	printf("top: %d, bottom: %d\n", top, bottom); */
-		/* 	usleep(50000); */
-		/* 	/\* getline(NULL, 0, stdin); *\/ */
-		/* } */
+		if (DEBUG == 1)
+		{
+			printf("top: %d, bottom: %d\n", top, bottom);
+			usleep(50000);
+			/* getline(NULL, 0, stdin); */
+		}
 	}
 }
