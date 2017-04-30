1798c08602c452f3e8ec9f2186004059daef627f
Daniel Alzugaray <dalzuga@gmail.com>
2017-04-29 22:45:51 -0700

    Adding FOV_ANGLE_HALF; moving poll_events().

--- src/main.c
+++ src/main.c
@@ -15,26 +15,3 @@ int main(void)
 
 	return (0);
 }
-
-int poll_events(void)
-{
-	SDL_Event event;
-	SDL_KeyboardEvent key;
-
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			return (1);
-		case SDL_KEYDOWN:
-			key = event.key;
-			/* If 'ESCAPE' is pressed */
-			if (key.keysym.scancode == 0x29)
-				return (1);
-			break;
-		}
-	}
-
-	return (0);
-}
