e48b6497520d4f65bc1c7c3448ece171453f8bb4
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-27 08:52:01 -0700

    Committing missing changes from 3 weeks ago.

--- src/game_functions.c
+++ src/game_functions.c
@@ -24,7 +24,7 @@ void game_start(SDL_Instance instance)
 	while(1)
 	{
 		/* sleep(1); 	/\* one second *\/ */
-		usleep(50000); /* 50ms */
+		usleep(30000); /* 30ms */
 		SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
 		SDL_RenderClear(instance.renderer);
 		if (poll_events() == 1)
@@ -33,7 +33,8 @@ void game_start(SDL_Instance instance)
 		paint_environment(instance, map, p);
 		fflush(stdout);
 		SDL_RenderPresent(instance.renderer);
-		(p->theta) += 5;
+		(p->theta) += 1;
+		p->theta = p->theta % 360;
 	}
 
 	/*
