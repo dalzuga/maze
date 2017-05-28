51440b20484224d7159eda4fd2e074f80ee3e16c
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-28 08:24:43 -0700

    aaaa

--- src/game_functions.c
+++ src/game_functions.c
@@ -22,22 +22,6 @@ void game_start(SDL_Instance instance)
 	start_position(&map, &p);
 	_print_map(map, 4, 4);
 
-	/* while(1) */
-	/* { */
-	/* 	/\* sleep(1); 	/\\* one second *\\/ *\/ */
-	/* 	usleep(30000); /\* 30ms *\/ */
-	/* 	SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0); */
-	/* 	SDL_RenderClear(instance.renderer); */
-	/* 	if (poll_events() == 1) */
-	/* 		break; 	/\* press ESC twice to exit a.out *\/ */
-	/* 	/\* draw_stuff(instance); *\/ */
-	/* 	paint_environment(instance, map, p); */
-	/* 	fflush(stdout); */
-	/* 	SDL_RenderPresent(instance.renderer); */
-	/* 	(p->theta) += 1; */
-	/* 	p->theta = p->theta % 360; */
-	/* } */
-
 	/*
 	 * TODO - need to code free_map() to free the map here
 	 */
