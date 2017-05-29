9eb69f5a1fa96a2136f763d0f16fdaca78d5d137
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-29 01:30:02 -0700

    cc - add calc_pxpy()

--- src/calc_functions.c
+++ src/calc_functions.c
@@ -9,10 +9,24 @@
 double calc_rclen(int **map, GamePlayer *p)
 {
 	/* declarations */
-	double rclen;
-	int px, py;
+	double rclen = -1;
+	int px = -1, py = -1;
 
 	/* inits */
+	calc_pxpy(&px, &py);
+
+	
+}
+
+/**
+ * calc_pxpy - calculates player position in the center of the
+ * player's block. This is necessary because we are using finer resolution to
+ * position player inside the block itself.
+ *
+ *
+ */
+void calc_pxpy(int *px, int *py)
+{
 	px = (p->x + 1) * BLOCK_UNITS - BLOCK_UNITS / 2;
 	py = (p->y + 1) * BLOCK_UNITS - BLOCK_UNITS / 2;
 
@@ -22,5 +36,4 @@ double calc_rclen(int **map, GamePlayer *p)
 		printf("p->x: %d, px: %d, ", p->x, *px);
 		printf("p->y: %d, py: %d\n", p->y, *py);
 	}
-
 }
