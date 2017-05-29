ca6c5a1a6e882b6e04892778133fe472af5a5f19
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-29 01:52:04 -0700

    ccc - game_func02()

--- src/game_func02.c
+++ src/game_func02.c
@@ -17,11 +17,36 @@ void game_func02(SDL_Instance instance, int **map, GamePlayer *p)
 	double rclen;		/* ray cast length */
 	int top, bottom;
 
-	rclen = calc_rclen(map, p);
+	for (i = 0; i < X_RES; i++)
+	{
+		rclen = calc_rclen(map, p);
+		col_height = WALL_HEIGHT * p->dpp / rclen;
+		top = Y_RES / 2 + col_height / 2;
+		bottom = Y_RES / 2 - col_height / 2;
 
-	/* inits */
-	col_height = WALL_HEIGHT * p->dpp / rclen;
+		if (DEBUG == 1)
+		{
+			printf("---------------4--------------\n");
+			printf("col_height: %f, top: %d, bottom: %d\n",
+			       col_height, top, bottom);
+			printf("i: %d, X_RES: %d\n", i, X_RES);
+			printf("i / X_RES: %f\n", (double) i / X_RES);
+			printf("FOV_ANGLE (radians): %d\n", FOV_ANGLE);
+			printf("FOV_ANGLE (degrees): %d\n",
+			       FOV_ANGLE * 180 / M_PI);
+			printf("------------------------------\n");
+			if (poll_events() == 1)
+			{
+				break;
+			}
+			usleep(SLEEP_TIME);
+		}
 
-	top = Y_RES / 2 + col_height / 2;
-	bottom = Y_RES / 2 - col_height / 2;
+		SDL_RenderDrawLine(instance.renderer, i, top, i, bottom);
+
+		if (DEBUG == 1)
+		{
+			SDL_RenderPresent(instance.renderer);
+		}
+	}
 }
