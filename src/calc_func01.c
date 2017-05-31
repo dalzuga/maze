2be287ee09eeb587d006794489b9b4997bc626ae
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-31 08:39:30 -0700

    c - move calc_pxpy to calc_intwrapper.

--- src/calc_func01.c
+++ src/calc_func01.c
@@ -23,23 +23,3 @@ double calc_func01(int **map __attribute__ ((unused)), GamePlayer *p, int i)
 
 	return (rclen);
 }
-
-/**
- * calc_pxpy - calculates player position in the center of the
- * player's block. This is necessary because we are using finer resolution to
- * position player inside the block itself.
- *
- *
- */
-void calc_pxpy(GamePlayer *p)
-{
-	p->px = (p->x + 1) * BLOCK_UNITS - BLOCK_UNITS / 2;
-	p->py = (p->y + 1) * BLOCK_UNITS - BLOCK_UNITS / 2;
-
-	if (DEBUG == 1)
-	{
-		printf("---------------2--------------\n");
-		printf("p->x: %d, px: %d, ", p->x, p->px);
-		printf("p->y: %d, py: %d\n", p->y, p->py);
-	}
-}
--- src/calc_intwrapper.c
+++ src/calc_intwrapper.c
@@ -47,3 +47,23 @@ double calc_alpha(int **map, GamePlayer *p, int i)
 
 	return (alpha);
 }
+
+/**
+ * calc_pxpy - calculates player position in the center of the player's
+ * block. This is necessary because we are using finer resolution to position
+ * player inside the block itself.
+ *
+ *
+ */
+void calc_pxpy(GamePlayer *p)
+{
+	p->px = (p->x + 1) * BLOCK_UNITS - BLOCK_UNITS / 2;
+	p->py = (p->y + 1) * BLOCK_UNITS - BLOCK_UNITS / 2;
+
+	if (DEBUG == 1)
+	{
+		printf("---------------2--------------\n");
+		printf("p->x: %d, px: %d, ", p->x, p->px);
+		printf("p->y: %d, py: %d\n", p->y, p->py);
+	}
+}
