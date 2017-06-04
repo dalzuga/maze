aa18cc77c8335b36fc9cbfecce2beeaee5405190
Daniel Alzugaray <dalzuga@gmail.com>
2017-06-04 11:58:46 -0700

    save WIP

--- src/proj_start.c
+++ src/proj_start.c
@@ -1,76 +1,36 @@
 #include "../headers/demo.h"
 
 /**
- * proj_where - determine where we are in the world
- *
- * @instance: SDL window and renderer instance
- * @map: address of map
- * @p: address of player
- *
- * Return: 0 if our location is valid, 1 otherwise.
- */
-int proj_where(SDL_Instance instance, int **map, GamePlayer *p)
-{
-	int Px, Py;
-
-	Px = p->x;
-	Py = p->y;
-
-	if (map[Px / 64][Py / 64] == 1)
-	{
-		/* we are inside a block */
-		return (1);
-	}
-
-	if (((Px / 64) >= map->rows) || ((Py / 64) >= map->cols))
-	{
-		/* we are outside the map */
-		return (1);
-	}
-
-	if (map[Px / 64][Py / 64] == 0)
-	{
-		/* we are at a valid location in the map */
-		return (0);
-	}
-}
-
-/**
  * proj_start - paints the environment from the player's POV
  *
  * @instance: SDL window and renderer instance
  * @map: address of map
  * @p: address of player
  *
+ * Return: 0 on success, 1 otherwise;
  */
 int proj_start(SDL_Instance instance, int **map, GamePlayer *p)
 {
 	int i, Px, Py, dhi;
 	double ap; /* alpha prime */
 
+	/* inits */
 	Px = p->x;
 	Py = p->y;
 	ap = (double) p->theta;
 
-	if (special_case(instance, ap) == 1)
+	if (proj_where(instance, map, p) == 1)
 	{
-		return (0);
+		return (1);
 	}
 
 	for (i = 0; i < X_RES; i++)
 	{
 		ap += (double) i / (double) FOV_ANGLE;
-		if (map[Px / 64][Py / 64] != 0)
+		if (special_case() == 0)
 		{
-			proj_d(instance, i, Y_RES);
-		}
-		else
-		{
-			if (special_case() == 0)
-			{
-				dhi = get_dhi(instance, p, map, ap);
-				proj_i(instance, i, dhi);
-			}
+			dhi = get_dhi(instance, p, map, ap);
+			proj_i(instance, i, dhi);
 		}
 
 		printf("dhi: %d\n", dhi);
---
+++ src/proj_where.c
@@ -0,0 +1,36 @@
+#include "../headers/demo.h"
+
+/**
+ * proj_where - determine where we are in the world
+ *
+ * @instance: SDL window and renderer instance
+ * @map: address of map
+ * @p: address of player
+ *
+ * Return: 0 if our location is valid, 1 otherwise.
+ */
+int proj_where(SDL_Instance instance, int **map, GamePlayer *p)
+{
+	int Px, Py;
+
+	Px = p->x;
+	Py = p->y;
+
+	if (map[Px / 64][Py / 64] == 1)
+	{
+		/* we are inside a block */
+		return (1);
+	}
+
+	if (((Px / 64) >= map->rows) || ((Py / 64) >= map->cols))
+	{
+		/* we are outside the map */
+		return (1);
+	}
+
+	if (map[Px / 64][Py / 64] == 0)
+	{
+		/* we are at a valid location in the map */
+		return (0);
+	}
+}
