da7610206599904b0427ec0b16cddb84c697a5ca
Daniel Alzugaray <dalzuga@gmail.com>
2017-06-05 15:31:24 -0700

    save WIP - hit first row of map {OK/OK}

--- src/calc_dhi.c
+++ src/calc_dhi.c
@@ -4,12 +4,11 @@
  * calc_dhi - finds the closest horizontal wall (x == C) hit by the ray
  *
  */
-double calc_dhi(GameMap *map __attribute__ ((unused)),
-		GamePlayer *p, int i)
+double calc_dhi(GameMap *map, GamePlayer *p, int i)
 {
 	/* declarations */
 	double ap;
-	int dy, Py, Ay;
+	int dy, Py, Ay, Ya, c, j, tmp;
 
 	/* inits */
 	ap = p->theta + (double) i / X_RES * FOV_ANGLE;
@@ -18,8 +17,26 @@ double calc_dhi(GameMap *map __attribute__ ((unused)),
 	Py = p->py;
 	dy = (64) * (Py/64) - Py - 1;
 	Ay = Py + dy;
+	Ya = -BLOCK_UNITS;
 
 	printf("Ay: %d\n", Ay);
+
+	for (c = 0; ; c++)
+	{
+		tmp = (Py + dy + c * Ya) / 64;
+		if (tmp < 0 || tmp > map->rows)
+		{
+			break;
+		}
+		else
+		{
+			j = tmp;
+		}
+	}
+
+	printf("map_size: (%d, %d)\n", map->cols, map->rows);
+	printf("c: %d\n", c);
+	printf("j: %d\n", j);
 
 	return (ap);
 }
