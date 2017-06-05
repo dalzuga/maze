b9a11d8d03979e156d4260d9490959f0ff01a797
Daniel Alzugaray <dalzuga@gmail.com>
2017-06-05 12:21:55 -0700

    save WIP

--- headers/prototypes.h
+++ headers/prototypes.h
@@ -1,6 +1,7 @@
-void game_start();
-int proj_start();
 int get_dhi(double ap);
-int dhi_i(int Px, int dy, int c, int Ya, double alpha);
-int dhi_j(int Px, int dy, int c, int Ya);
-int dhi_dhi(int Px, int dy, int c, int Ya, double alpha);
+int dhi_i(int Px,int dy,int c,int Ya,double alpha);
+int dhi_j(int Py,int dy,int c,int Ya);
+int dhi_dhi(int Px,int dy,int c,int Ya,double alpha);
+void game_start();
+int main(void);
+int proj_start();
