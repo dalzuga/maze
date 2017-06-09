3ae5b55389db94d38708b8c0f1cf02d56fefa2de
Daniel Alzugaray <dalzuga@gmail.com>
2017-06-09 14:50:09 -0700

    save test - {OK/OK}

--- headers/demo.h
+++ headers/demo.h
@@ -16,10 +16,10 @@
 #define PLAYER_HEIGHT WALL_HEIGHT / 2
 #define ANGLE_STEP 1
 #define MAP_ROWS 10
-#define MAP_COLS 10
+#define MAP_COLS 30
 #define START_X 198
-#define START_Y 198
-#define START_THETA 89
+#define START_Y (198 + 96)
+#define START_THETA 60
 
 typedef struct SDL_Instance
 {
