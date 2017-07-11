d581b354986f93c15cc5e3072ddc8d8ab9a86aa4
Daniel Alzugaray <dalzuga@gmail.com>
2017-07-10 20:34:06 -0700

    save WIP - using gdb to track bug {OK/KO}

--- src/map_functions.c
+++ src/map_functions.c
@@ -84,6 +84,16 @@ void init_map(GameMap *map)
 			}
 		}
 	}
+
+	for (i = 0; i < map->rows; i++)
+	{
+		for (j = 0; j < map->cols; j++)
+		{
+			printf("[%d]", map->array[i][j]);
+		}
+		printf("\n");
+	}
+
 }
 
 /**
@@ -104,30 +114,23 @@ void _print_map(GameMap *map, GamePlayer *p)
 }
 
 /**
- * print_map - prints a map
+ * print_map - prints a map with the player positioned in it
  * @array - map array to print
  * @rows - number of rows
  * @cols - number of columns
  *
  * Return: Nothing.
  */
-void print_map(GameMap *map, GamePlayer *p)
+void print_map(GameMap *map, GamePlayer __attribute__((unused)) *p)
 {
 	int j, i;
-	int Px = p->px, Py = p->py;
+	/* int Px = p->px, Py = p->py; */
 
 	for (j = 0; j < map->rows; j++)
 	{
 		for (i = 0; i < map->cols; i++)
 		{
-			if (i == Px / BLOCK_UNITS && j == Py / BLOCK_UNITS)
-			{
-				cprintf(31, 46, "%s", "P");
-			}
-			else
-			{
-				printf("%d", map->array[j][i]);
-			}
+			printf("%d", map->array[j][i]);
 		}
 		printf("\n");
 	}
