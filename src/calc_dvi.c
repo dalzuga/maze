4731c0e66188e4d5d6c51d11a624dbc09b8a4934
Daniel Alzugaray <dalzuga@gmail.com>
2017-08-14 19:02:24 -0700

    save WIP - boilerplate for calc_dvi() {KO}

--- src/calc_dvi.c
+++ src/calc_dvi.c
@@ -8,108 +8,30 @@ double calc_dvi(GameMap *map, GamePlayer *p, int ppcs4715)
 {
 	/* declarations */
 	double ap;
-	int By __attribute__((unused));
-	int Px, Py, Ya, Xa, c, i, j;
+	int dx;
+	int Px, Py, c, i, j;
+	int Xa;
+	int flag;
+	int dist;
 
 	/* inits */
 	ap = p->theta + (double) ppcs4715 / X_RES * FOV_ANGLE;
 	ap = calc_mod360(ap);
+	Py = p->py;
+	Px = p->px;
+	flag = 0;
+	dist = 0;
+	/* end inits */
 
 	if (axis_angle(ap))	/* special cases: 0, 90, 180, 270 */
 	{
 		return (special_dhi(map, p, ap, ppcs4715));
 	}
 
-	Py = p->py;
-	Px = p->px;
-	/* end inits */
+	dx = calc_dx(p, ap);
+	Xa = calc_Xa(ap);
 
-	/* `Xa` signed horizontal direction */
-	/* right */
-	if (ap < 180)
-	{
-		Xa = 64;
-	}
-	/* left */
-	else			/* guaranteed --> (ap > 180) */
-	{
-		Xa = -64;
-	}
-
-	/* Ya - vertical distance of the ray for each block */
-	Ya = fabs(Xa / tan(ap * M_PI / 180));
-
-	/* put the proper signs on `Ya` component */
-	/* down */
-	if (90 < ap && ap < 270)
-	{
-	}
-	/* up */
-	else 			/* guaranteed --> ap < 90 || ap > 270  */
-	{
-		Ya = -Ya;
-	}
-
-	/* `c` is the number of blocks crossed */
-	for (c = 0; ; c++)
-	{
-		i = Px + c * Xa;
-		j = Py + c * Ya;
-
-		if (j/64 < 1 || j/64 > map->rows - 2)
-		{
-			/* bring back `j` since it's outside the map boundary */
-			/* down */
-			if (90 < ap && ap < 270)
-			{
-				if (j > map->rows * 64)
-				{
-					j = (map->rows - 1) * 64;
-				}
-			}
-			/* up */
-			else if (ap < 90 || ap > 270)
-			{
-				if (j < 64)
-				{
-					j = 64 - 1;
-				}
-			}
-			else	/* this shouldn't happen */
-			{
-				j = Py;
-			}
-
-			if (DEBUG >= 3)
-			{
-				print_map(map, p);
-				rcprint_map(map, p, j/64, i/64);
-				printf("horizontal border exceeded.\n");
-			}
-			break;
-		}
-
-		if (i/64 < 1 || i/64 > map->cols - 2)
-		{
-			if (DEBUG >= 3)
-			{
-				print_map(map, p);
-				rcprint_map(map, p, j/64, i/64);
-				printf("vertical border exceeded.\n");
-			}
-			break;
-		}
-
-		if (map->array[j/64][i/64] == 1)
-		{
-			if (DEBUG >= 2)
-			{
-				print_map(map, p);
-				rcprint_map(map, p, j/64, i/64);
-			}
-			break;
-		}
-	}
+	i = Px + dx;
 
 	if (DEBUG >= 2)
 	{
