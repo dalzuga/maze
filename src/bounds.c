71827224ccce0c8a38b9cfa8d86483b505dc982d
Daniel Alzugaray <dalzuga@gmail.com>
2017-09-01 13:18:24 -0700

    save WIP - woohoo! bug fixed {OK}

--- src/bounds.c
+++ src/bounds.c
@@ -5,8 +5,7 @@ int check_blocks(MazeStruct *maze, int i, int j)
 	if (check_map_bounds(maze, i, j))
 		return (0);
 
-	/* check for a wall */
-	if (maze->map->array[j/64][i/64] == 0)
+	if (check_wall(maze, i, j))
 	{
 		return (1);
 	}
@@ -24,4 +23,18 @@ int check_map_bounds(MazeStruct *maze, int i, int j)
 	}
 
 	return (0);
+}
+
+int check_wall(MazeStruct *maze, int i, int j)
+{
+	/* check for a wall */
+	if (maze->map->array[j/64][i/64] == 1
+	    || maze->map->array[(j-1)/64][i/64] == 1
+	    || maze->map->array[j/64][(i-1)/64] == 1
+	    || maze->map->array[(j-1)/64][(i-1)/64] == 1)
+	{
+		return (0);
+	}
+
+	return (1);
 }
